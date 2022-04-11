0352b0ce37e289506ff4b1d718ebdaa06731e02a
poojabasavanal <poojabasavanal@gmail.com>
2022-04-11 12:30:51 +0530

    commit

---
+++ sampleCFcoreMVC/Migrations/20220411061514_seeddata.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace sampleCFcoreMVC.Migrations
+{
+    public partial class seeddata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "TodoItem",
+                columns: new[] { "Id", "Description", "IsComplete", "Name" },
+                values: new object[] { 1L, "In meeting need to discuss some points.", true, "meeting with management" });
+
+            migrationBuilder.InsertData(
+                table: "TodoItem",
+                columns: new[] { "Id", "Description", "IsComplete", "Name" },
+                values: new object[] { 2L, "List of thisitem buy.", false, "Go for shopping" });
+
+            migrationBuilder.InsertData(
+                table: "TodoItem",
+                columns: new[] { "Id", "Description", "IsComplete", "Name" },
+                values: new object[] { 3L, "here is task to ask to do on call", true, "call to someone for do some task" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TodoItem",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "TodoItem",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "TodoItem",
+                keyColumn: "Id",
+                keyValue: 3L);
+        }
+    }
+}
---
+++ sampleCFcoreMVC/Models/TodoContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sampleCFcoreMVC.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options): base(options)
+        {
+
+        }
+        public DbSet<TodoItem> TodoItem { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>().HasData(
+                new TodoItem() { Id = 1, Name = "meeting with management", IsComplete = true, Description = "In meeting need to discuss some points." },
+                new TodoItem() { Id = 2, Name = "Go for shopping", IsComplete = false, Description = "List of thisitem buy." },
+                new TodoItem() { Id = 3, Name = "call to someone for do some task", IsComplete = true, Description = "here is task to ask to do on call" });
+                }
+        }
+
+    }
+
+
